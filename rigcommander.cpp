3383092dd73bc8895d97281d0e7872104397435a
Phil Taylor <phil@m0vse.uk>
2021-03-04 23:44:22 +0000

    Update rigcommander.cpp

--- rigcommander.cpp
+++ rigcommander.cpp
@@ -72,6 +72,8 @@ void rigCommander::commSetup(unsigned char rigCivAddr, QString rigSerialPort, qu
 
 }
 
+
+
 void rigCommander::commSetup(unsigned char rigCivAddr, udpPreferences prefs)
 {
     // construct
